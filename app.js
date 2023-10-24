e6a321da9a4c9ee8afa5ebbbfd52d3f0c6c052a4
Pizaña Serrano Jose Eliu <73567549+JoseChiquito@users.noreply.github.com>
2023-10-23 21:06:20 -0600

    app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
-app.use(express.static('/RA1_PSJE/src/'));
+app.use(express.static(__dirname + '/RA1_PSJE/src')); 
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/RA1_PSJE/src/index.html');
 });
 
 app.listen(port, ()=>{
-    console.log("Sevidor coriendo en puerto: ${port}");
+    console.log(`Servidor corriendo en puerto: ${port}`); 
 });
 
 //app.get('*',(req,res)=>{
